019ecae4f42eeecc35bb3ff9bf649e65502cf4fb
Sledg130 <115500564+Sledg130@users.noreply.github.com>
2023-02-19 23:40:21 +0300

    Update task1.py

--- Итоговое задание/task1.py
+++ Итоговое задание/task1.py
@@ -6,13 +6,16 @@ class Fighter:
     Класс описывает героя Воина.
     """
 
-    def __init__(self, name: str, lvl: int, strength: int, ExperiencePoints: int):
+    def __init__(self, name: str, lvl: int, strength: int, experience_points: int):
         """
         Инициализация экземпляра класса.
         :param name: Имя героя
         :param lvl: Уровень героя
         :param strength: Сила героя
         :param ExperiencePoints: Опыт героя
+
+        Пример:
+        >>> fighter1 = Fighter("Джеймс", 1, 16, 0)
         """
         if not isinstance(name, str):
             raise TypeError("Имя должно быть типа str")
@@ -22,70 +25,89 @@ class Fighter:
             raise TypeError("Уровень должен быть типа int")
         if lvl <= 0:
             raise ValueError("Уровень должен быть положительным числом")
-        self.lvl = lvl
+        self._lvl = lvl
 
         if not isinstance(strength, int):
             raise TypeError("Сила должна быть типа int")
         if strength <= 0:
             raise ValueError("Сила должна быть положительным числом")
-        self.strength = strength
+        self._strength = strength
 
-        if not isinstance(ExperiencePoints, int):
+        if not isinstance(experience_points, int):
             raise TypeError("Опыт должен быть типа int")
-        if ExperiencePoints < 0:
+        if experience_points < 0:
             raise ValueError("Опыт не может быть отрицательным числом")
-        self.ExperiencePoints = ExperiencePoints
+        self._experience_points = experience_points
 
     def __str__(self):
         """
         Метод возвращает строку для чтения.
+
+        Пример:
+        >>> fighter1 = Fighter("Джеймс", 1, 16, 0)
+        >>> print(fighter1)
         """
-        return f"Воин {self.name}; Уровень {self.lvl}; Сила {self.strength}; Опыт {self.ExperiencePoints}"
+        return f"Воин {self.name}; Уровень {self._lvl}; Сила {self._strength}; Опыт {self._experience_points}"
 
     def __repr__(self):
         """
         Метод возвращает строку, показывающую, как может быть инициализирован экземпляр.
+
+        Пример:
+        >>> fighter1 = Fighter("Джеймс", 1, 16, 0)
+        >>> print(repr(fighter1))
         """
-        return f"{self.__class__.__name__}(Имя={self.name!r}, Уровень={self.lvl!r}, Сила={self.strength!r}, Опыт={self.ExperiencePoints!r})"
+        return f"{self.__class__.__name__}(Имя={self.name!r}, Уровень={self._lvl!r}, Сила={self._strength!r}, Опыт={self._experience_points!r})"
 
     def lvl_up(self):
         """
-        Метод определяет уровень героя по имеющемуся опыту.
+        Метод определяет уровень героя по имеющемуся опыту. Если опыт достигает определённого уровня, то атрибут lvl меняется.
+
+        Пример:
+        >>> fighter1.lvl_up()
         """
         Experience_of_lvl = [0, 300, 900, 2700, 6500, 14400, 23000, 34000, 48000, 64000, 85000, 100000]
-        for exp in Experience_of_lvl:
-            if self.ExperiencePoints < exp:
-                self.lvl = Experience_of_lvl.index(exp)
-                print("Уровень героя", self.lvl)
-                break
-        return self.lvl
 
-    def exp_up(self, Experience: int):
+        for exp in Experience_of_lvl:
+            if self._experience_points < exp:
+                self._lvl = Experience_of_lvl.index(exp)
+                print("Уровень героя", self._lvl)
+                break
+
+        return self._lvl
+
+    def exp_up(self, experience: int):
         """
         Метод увеличивает количество имеющегося опыта.
+
+        Пример:
+        >>> fighter1.exp_up(1000)
         """
-        self.Experience = Experience
-        self.ExperiencePoints += self.Experience
-        print("Получено опыта", self.ExperiencePoints)
-        return self.ExperiencePoints
+        self.experience = experience
+        self._experience_points += self.experience
+        print("Получено опыта", self._experience_points)
+        return self._experience_points
 
     def attack(self):
         """
         Метод позволяет провести атаку по цели. Удар считается успешным, если число на кубике больше 10.
+
+        Пример:
+        >>> fighter1.attack()
         """
         hit = random.randint(1, 20)
         print("Бросаем кубик... Выпало:", hit)
         if hit > 10:
-            print("Попадание")
+            print(True)
         else:
-            print("Промах")
+            print(False)
 
 class Knight(Fighter):
     """
     Документация на дочерний класс.
     Класс описывает подкласс героя Воина -- Рыцаря.
     """
-    def __init__(self, name: str, lvl: int, strength: int, ExperiencePoints: int, Inspiration = None):
+    def __init__(self, name: str, lvl: int, strength: int, experience_points: int, inspiration = None):
         """
         Инициализация экземпляра подкласса.
         :param name: Имя героя
@@ -93,37 +115,51 @@ class Knight(Fighter):
         :param strength: Сила героя
         :param ExperiencePoints: Опыт героя
         :param Inspiration: Вдохновение. Приравнивается уровню или задаётся вручную.
-        """
-        super().__init__(name, lvl, strength, ExperiencePoints)
-        if Inspiration is None:
-            Inspiration = self.lvl
-        self.Inspiration = Inspiration
 
-        if not isinstance(Inspiration, int):
+        Пример:
+        >>> knight1 = Knight("Джеймс", 1, 16, 0, 2)
+        """
+        super().__init__(name, lvl, strength, experience_points)
+        if inspiration is None:
+            inspiration = self._lvl
+        self._inspiration = inspiration
+
+        if not isinstance(inspiration, int):
             raise TypeError("Вдохновение должно быть типа int")
-        if Inspiration <= 0:
+        if inspiration <= 0:
             raise ValueError("Вдохновение должно быть положительным числом")
 
     def __str__(self):
         """
         Метод возвращает строку для чтения.
         Перегрузку метода реализовали из-за необходимости указания подкласса героя.
+
+        Пример:
+        >>> knight1 = Knight("Джеймс", 1, 16, 0, 2)
+        >>> print(knight1)
         """
         s_str = super().__str__()
-        return f"{s_str}; Подкласс: {self.__class__.__name__}"
+        return f"{s_str}; Вдохновение: {self._inspiration}; Подкласс: {self.__class__.__name__}"
 
     def __repr__(self):
         """
         Метод возвращает строку, показывающую, как может быть инициализирован экземпляр.
         Перегрузку метода реализовали из-за необходимости указания подкласса героя.
+
+        Пример:
+        >>> knight1 = Knight("Джеймс", 1, 16, 0, 2)
+        >>> print(repr(knight1))
         """
         s_repr = super().__repr__()
-        return f"{s_repr}; Подкласс={self.__class__.__name__!r}"
+        return f"{s_repr}; Вдохновение={self._inspiration!r}; Подкласс={self.__class__.__name__!r}"
 
     def attack(self):
         """
         Метод позволяет провести атаку по цели. Удар считается успешным, если число на кубике больше 10.
         Перегрузка метода реализовали из-за того, что подкласс Рыцарь имеет возможность провести второй удар.
+
+        Пример:
+        >>> knight1.attack()
         """
         s_attack = super().attack()
         hit_2 = random.randint(1, 20)
@@ -132,19 +168,18 @@ class Knight(Fighter):
         if answer == "Да":
             print("Бросаем кубик... Выпало:", hit_2)
             if hit_2 > 10:
-                print("Попадание")
+                print(True)
             else:
-                print("Промах")
+                print(False)
         elif answer == "Нет":
             print("Дополнительную атаку не провели")
         else:
             print("Не могу тебя понять")
 
 if __name__ == "__main__":
-    Fighter = Fighter("Джеймс", 1, 16, 0)
-    #Fighter.lvl_up()
-    #Fighter.exp_up(1000)
-    Knight = Knight("Джеймс", 3, 16, 899)
-    #Knight.attack()
+    fighter1 = Fighter("Джеймс", 1, 16, 0)
+    #fighter1.attack()
+    knight1 = Knight("Джеймс", 3, 16, 899)
+    #knight1.attack()
     pass
 
